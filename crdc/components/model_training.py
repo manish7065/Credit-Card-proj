7ea084e92d4fb9a2b7fa78429a9cf641bb2576f7
Manish Kumar <manumi296@gmail.com>
2023-06-02 23:44:50 +0530

    model training goin in

---
+++ crdc/components/model_training.py
@@ -0,0 +1,14 @@
+from crdc.exception import CrdcException
+from crdc.logger import logging
+
+import os,sys
+
+class ModelTraining():
+    def __init__(self) -> None:
+        pass
+
+    def initiate_model_training(self):
+        try:
+            pass
+        except Exception as e:
+            raise CrdcException(e,sys)
