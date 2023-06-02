0af77f4efb78ffe0552887370b84d97c3642b561
Manish Kumar <manumi296@gmail.com>
2023-06-02 23:42:23 +0530

    trial of data transformation

--- trial.py
+++ trial.py
@@ -1,6 +1,10 @@
 from crdc.components.data_ingestion import DataIngestion
+from crdc.components.data_transformation import DataTransformation
 
-di=DataIngestion()
+# di=DataIngestion()
 
-idi=di.initiate_data_ingestion()
-print(f"data stored:{idi}")
+# idi=di.initiate_data_ingestion()
+# print(f"data stored:{idi}")
+
+dt = DataTransformation()
+dt.initiate_data_transformation()
