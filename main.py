83b1131414a0cd98c81723cd32ba95f9def0e17a
Manish Kumar <manumi296@gmail.com>
2023-06-03 23:36:25 +0530

    addwd flask app

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print('Hello World')
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# Replace this with your machine learning model or function
+def predict_default(inputs):
+    # Your code for predicting default goes here
+    # Return "Will Default" or "Will Not Default" based on the prediction
+    return "Will Default"  # Placeholder result
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Get user inputs from the form
+        inputs = []
+        for i in range(1, 11):
+            input_name = f'input{i}'
+            input_value = request.form.get(input_name)
+            inputs.append(input_value)
+        
+        # Perform prediction using the machine learning model
+        prediction = predict_default(inputs)
+        
+        # Render the result template with the prediction
+        return render_template('result.html', prediction=prediction)
+    
+    # Render the input form template initially
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
